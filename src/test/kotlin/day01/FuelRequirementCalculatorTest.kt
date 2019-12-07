16566d8f4de8d23efa4599241795ac8f05e8e555
David Williams <david.williams@autotrader.co.uk>
2019-12-07 09:35:18 +0000

    Rename test

--- src/test/kotlin/day01/FuelRequirementCalculatorTest.kt
+++ src/test/kotlin/day01/FuelRequirementCalculatorTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 
-class Day01Test {
+class FuelRequirementCalculatorTest {
 
     private val testSubject: FuelRequirementCalculator = FuelRequirementCalculator()
 
