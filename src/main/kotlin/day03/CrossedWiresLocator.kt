85b405f1a2d1466a56aec70afffa15e25993f625
David Williams <david.williams@autotrader.co.uk>
2019-12-08 20:10:28 +0000

    Solution for day03 part 2

--- src/main/kotlin/day03/CrossedWiresLocator.kt
+++ src/main/kotlin/day03/CrossedWiresLocator.kt
@@ -6,7 +6,7 @@ import kotlin.math.abs
 class CrossedWiresLocator {
 
     fun getCrossedWiresDistance(wirePath1: String, wirePath2: String): Int {
-        val circuit = hashMapOf<Coordinate, Set<Int>>()
+        val circuit = hashMapOf<Coordinate, MutableSet<Point>>()
         plotWire(circuit, wirePath1, 1)
         plotWire(circuit, wirePath2, 2)
 
@@ -14,8 +14,18 @@ class CrossedWiresLocator {
         return crossedWireLocations.map { abs(it.x) + abs(it.y) }.min()!!
     }
 
-    private fun plotWire(circuit: Circuit, wirePath: String, pathNum: Int) {
+    fun getMinimumStepsIntersection(wirePath1: String, wirePath2: String): Int {
+        val circuit = hashMapOf<Coordinate, MutableSet<Point>>()
+        plotWire(circuit, wirePath1, 1)
+        plotWire(circuit, wirePath2, 2)
+
+        val crossedWireLocations = circuit.filterValues { it.size > 1 }.values
+        return crossedWireLocations.map { it.fold(0) { acc: Int, p2: Point -> acc + p2.stepNo } }.min()!!
+    }
+
+    private fun plotWire(circuit: Circuit, wirePath: String, wireNo: Int) {
         val currentLocation = Coordinate(0, 0)
+        var stepNo = 0
 
         wirePath.split(",").forEach {
             val direction = it[0]
@@ -30,33 +40,56 @@ class CrossedWiresLocator {
             }
 
             for (i in 0 until distance) {
-                if (currentLocation.isNotOrigin()) circuit.merge(currentLocation.copy(), setOf(pathNum)) { x, y -> x + y }
+                if (currentLocation.isNotOrigin()) circuit.merge(currentLocation.copy(), mutableSetOf(Point(stepNo, wireNo))) { x, y -> x.addAll(y); x }
                 currentLocation += operator
+                stepNo++
             }
-        }
-    }
-
-    data class Coordinate(var x: Int, var y: Int) {
-        operator fun plusAssign (diff: Pair<Int, Int>) {
-            x += diff.first
-            y += diff.second
-        }
-
-        fun isNotOrigin() = !(x == 0 && y == 0)
-
-        override fun toString(): String {
-            return "($x, $y)"
         }
     }
 
 }
 
-typealias Circuit = MutableMap<CrossedWiresLocator.Coordinate, Set<Int>>
+class Point(val stepNo: Int, val wireNo: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Point
+
+        if (wireNo != other.wireNo) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return wireNo
+    }
+
+    override fun toString(): String {
+        return "stepNo: $stepNo, wireNo: $wireNo"
+    }
+}
+
+data class Coordinate(var x: Int, var y: Int) {
+    operator fun plusAssign (diff: Pair<Int, Int>) {
+        x += diff.first
+        y += diff.second
+    }
+
+    fun isNotOrigin() = !(x == 0 && y == 0)
+
+    override fun toString(): String {
+        return "($x, $y)"
+    }
+}
+
+typealias Circuit = MutableMap<Coordinate, MutableSet<Point>>
 
 fun main() {
     val crossedWiresLocator = CrossedWiresLocator()
     val input = File("src/main/resources/day03/input.txt").readLines()
-    print(crossedWiresLocator.getCrossedWiresDistance(input[0], input[1]))
+    println(crossedWiresLocator.getCrossedWiresDistance(input[0], input[1]))
+    println(crossedWiresLocator.getMinimumStepsIntersection(input[0], input[1]))
 }
 
 
--- src/test/kotlin/day03/CrossedWiresLocatorTest.kt
+++ src/test/kotlin/day03/CrossedWiresLocatorTest.kt
@@ -10,11 +10,21 @@ class CrossedWiresLocatorTest {
 
     @ParameterizedTest
     @CsvSource("'R8,U5,L5,D3', 'U7,R6,D4,L4', 6",
-        "'R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83', 159", // wire crosses with itself?
+        "'R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83', 159",
         "'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7', 135")
-    fun `calculateCrossedWiresDistance should return the coordinates of the closest cross to the origin`(wire1Path: String, wire2Path: String, expectedDistance: Int) {
-        val actual = testSubject.getCrossedWiresDistance(wire1Path, wire2Path)
+    fun `getCrossedWiresDistance should return the distance of the closest intersection to the origin`(wirePath1: String, wirePath2: String, expectedDistance: Int) {
+        val actual = testSubject.getCrossedWiresDistance(wirePath1, wirePath2)
 
         assertEquals(expectedDistance, actual)
+    }
+
+    @ParameterizedTest
+    @CsvSource("'R8,U5,L5,D3', 'U7,R6,D4,L4', 30",
+        "'R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83', 610",
+        "'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7', 410")
+    fun `findMinStepsIntersection should find the intersection with the smallest number of steps`(wirePath1: String, wirePath2: String, expected: Int) {
+        val actual = testSubject.getMinimumStepsIntersection(wirePath1, wirePath2)
+
+        assertEquals(expected, actual)
     }
 }
