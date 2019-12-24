271466a536b3388856cdf86d76754a89ee2bef8f
David Williams <david.williams@autotrader.co.uk>
2019-12-24 22:32:29 +0100

    Day 09 - Part 2

--- src/main/kotlin/day09/BasicOperationOfSystemTest.kt
+++ src/main/kotlin/day09/BasicOperationOfSystemTest.kt
@@ -4,14 +4,22 @@ import intcode.IntcodeComputer
 
 class BasicOperationOfSystemTest {
 
-    fun runDiagnostic(program: MutableList<Long>) {
+    fun runDiagnostic(program: List<Long>) {
         val intcodeComputer = IntcodeComputer(program, mutableListOf(1))
-        val output = intcodeComputer.calculateIntcode()
-        println(output)
+        intcodeComputer.calculateIntcode()
+        println(intcodeComputer.getOutput())
+    }
+
+    fun boostSensors(program: List<Long>) {
+        val intcodeComputer = IntcodeComputer(program, mutableListOf(2))
+        intcodeComputer.calculateIntcode()
+        println(intcodeComputer.getOutput())
     }
 }
 
 fun main() {
     val program = IntcodeComputer.readInput("src/main/resources/day09/input.txt")
-    BasicOperationOfSystemTest().runDiagnostic(program.toMutableList())
+    val boost = BasicOperationOfSystemTest()
+    boost.runDiagnostic(program)
+    boost.boostSensors(program)
 }
